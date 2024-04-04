2ea7f735f5dad60252987ddf94aecec3c5f9c5eb
yecrystal <crystalllye@gmail.com>
2024-04-04 01:32:21 -0400

    Moving pieces

--- chessMain.py
+++ chessMain.py
@@ -31,10 +31,25 @@ def main():
     gs = GameState()
     loadImages() # Only once before the while loop
     running = True
+    sqSelected = () # No square is selected initially, keep track of last click of the user (tuple: (row, col)))
+    playerClicks = [] # Keep track of the player clicks (two tuples: [(6, 4), (4, 4)])
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN: 
+                location = p.mouse.get_pos() # Get (x, y) location of mouse
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if sqSelected == (row, col): # The user clicked the same square twice
+                    sqSelected = () # Deselect
+                    playerClicks = [] # Clear player clicks
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected) # Append for both 1st and 2nd clicks
+                if len(playerClicks) == 2: # After 2nd click
+                    
+
         drawGameState(screen, gs.board)  # Pass gs.board instead of gs to the drawGameState function
         clock.tick(MAX_FPS)
         p.display.flip()
@@ -49,7 +64,7 @@ def drawGameState(screen, board):
 Draw the squares on the board
 '''
 def drawBoard(screen):
-    colours = [p.Color("white"), p.Color("gray")]
+    colours = [p.Color("white"), p.Color("pink")]
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             colour = colours[((r+c)%2)]
